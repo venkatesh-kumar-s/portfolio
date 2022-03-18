e62b3b970ee64ef72458eefca59aa55ea4a21387
venkatesh kumar <venkatesh.k@ampcome.com>
2022-03-19 01:35:05 +0530

    Sections Content initialised

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,12 +15,27 @@ const Home = () => {
   return (
     <div className="mt-4">
       <div className="section"></div>
-      <div data-aos="fade-up" className="section"></div>
-      <div data-aos="fade-up" className="section"></div>
-      <div data-aos="fade-up" className="section"></div>
-      <div data-aos="fade-up" className="section"></div>
-      <div data-aos="fade-up" className="section"></div>
-      <div data-aos="fade-right" className="section"></div>
+      <div data-aos="fade-up" className="section">
+        where do you see yourself in 5 years
+      </div>
+      <div data-aos="fade-right" className="section">
+        what kind of work environment do you like most?
+      </div>
+      <div data-aos="fade-left" className="section">
+        what is the professional experience you are most proud of?
+      </div>
+      <div data-aos="fade-right" className="section">
+        what are you looking for in a new position?
+      </div>
+      <div data-aos="fade-left" className="section">
+        strengths & weakness
+      </div>
+      <div data-aos="fade-right" className="section">
+        How would you boos or coworkers describe you?
+      </div>
+      <div data-aos="fade-left" className="section">
+        How would you deal with pressure or stressful situations?
+      </div>
     </div>
   );
 };
