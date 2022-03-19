501a5d2e538892fe336054eb9528894ee6f4ab8d
venkatesh kumar <venkatesh.k@ampcome.com>
2022-03-19 17:26:37 +0530

    Added Navbar collapse functionality

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 //Bootstrap Components
 import { Container, Navbar } from "react-bootstrap";
@@ -13,16 +13,18 @@ import { Menu } from "../data/menu";
 import TechStack from "./TechStack";
 
 const Nav = () => {
+  const [state, setState] = useState(false);
   return (
     <Navbar
       collapseOnSelect
       bg="dark"
       expand="lg"
+      expanded={state}
       variant="dark"
       //fixed="top"
       className="shadow bg-black"
     >
-      <Container fluid className="row mx-sm-0">
+      <Container fluid className="row mx-sm-0 ps-3 pe-1 px-md-4">
         <Link to="/" className="navbar-brand col">
           <span className="text-warning">V</span>enkatesh Kumar
         </Link>
@@ -30,14 +32,23 @@ const Nav = () => {
         <div className="col d-none d-md-block">
           <TechStack />
         </div>
-        <NavbarToggle aria-controls="navbar-hook" className="col-auto" />
+        <NavbarToggle
+          aria-controls="navbar-hook"
+          className="col-auto p-0"
+          onClick={() => setState(!state)}
+        />
         <NavbarCollapse
           id="navbar-hook"
           className="col-auto justify-content-end"
         >
           <nav className="nav navbar-nav">
             {Menu?.map((r, i) => (
-              <Link key={i} className="nav-link" to={r?.path}>
+              <Link
+                key={i}
+                className="nav-link"
+                to={r?.path}
+                onClick={() => setState(false)}
+              >
                 <img
                   src={r?.icon}
                   style={{ objectFit: "contain", width: 20, marginRight: 5 }}
