a22fbc06d91a1d898980c89c28b29f0b5c9e9b4c
venkatesh kumar <venkatesh.k@ampcome.com>
2022-03-27 00:19:23 +0530

    Added Skills Progress Frame

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import { Avatar } from "@mui/material";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import React, { useEffect } from "react";
-import { Container } from "react-bootstrap";
+import { Container, ProgressBar } from "react-bootstrap";
 
 import "../components/components.css";
 import Introduction from "../components/Introduction";
@@ -22,6 +22,34 @@ const Home = () => {
       </div>
       <div data-aos="fade-up" className="statistics-section m-auto d-flex">
         <LandingStatistics />
+      </div>
+      <div
+        data-aos="fade-up"
+        className="bg-info px-4 mx-auto my-5 skills-rating"
+      >
+        <div className="row mx-auto">
+          <ProgressBar
+            className="col-sm-12 col-md-5 p-0 my-4 mx-2"
+            label="40%"
+            variant="success"
+            now={40}
+          />
+          <ProgressBar
+            className="col-sm-12 col-md-5 p-0 my-4 mx-2"
+            variant="info"
+            now={20}
+          />
+          <ProgressBar
+            className="col-sm-12 col-md-5 p-0 my-4 mx-2"
+            variant="warning"
+            now={60}
+          />
+          <ProgressBar
+            className="col-sm-12 col-md-5 p-0 my-4 mx-2"
+            variant="danger"
+            now={80}
+          />
+        </div>
       </div>
       <Container>
         <div data-aos="fade-up" className="questions-section">
