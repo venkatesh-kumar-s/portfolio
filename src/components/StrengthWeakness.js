e884b01f0888eb7c71570b7100005cd1a4b5b8fa
venkatesh kumar <venkatesh.k@ampcome.com>
2022-03-27 17:03:15 +0530

    strength weakness modifies

---
+++ src/components/StrengthWeakness.js
@@ -0,0 +1,55 @@
+import { Avatar } from "@mui/material";
+import React from "react";
+import { Container } from "react-bootstrap";
+
+import quoteRight from "../assets/images/quote-right.png";
+import quoteLeft from "../assets/images/quoteLeft.png";
+import "../components/components.css";
+
+const StrengthWeakness = () => {
+  return (
+    <div className="strengths-weakness">
+      <div className="strengths">
+        <Container fluid className="px-0">
+          <h1 className="d-flex">
+            <Avatar src={quoteLeft} sx={{ height: 60, width: 60 }} />
+            &nbsp; <span className="my-auto">Strengths . . . </span>
+          </h1>
+          <ul
+            className="my-5 my-md-4"
+            style={{ fontWeight: 500, listStyle: "none" }}
+          >
+            <li>- Hungry for upgrades</li>
+            <li>- Able to work alone</li>
+            <li>- Great Team Player</li>
+            <li>- Confidence</li>
+            <li>- Creativity</li>
+          </ul>
+        </Container>
+      </div>
+      <div className="weakness bg-transparent">
+        <Container
+          fluid
+          className="text-end"
+          style={{
+            position: "absolute",
+            bottom: 20,
+            right: 20,
+          }}
+        >
+          <ul className="mb-5" style={{ fontWeight: 500 }}>
+            <li>Curiosity -</li>
+            <li>Seek for perfection -</li>
+            <li>Strings attached to trends -</li>
+          </ul>
+          <h1 className="d-flex float-end">
+            <span className="my-auto">. . . Weakness </span>&nbsp;{" "}
+            <Avatar src={quoteRight} sx={{ height: 60, width: 60 }} />
+          </h1>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default StrengthWeakness;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,9 +8,7 @@ import "../components/components.css";
 import Introduction from "../components/Introduction";
 import LandingStatistics from "../components/LandingStatistics";
 import Skills from "../components/Skills";
-
-import quoteRight from "../assets/images/quote-right.png";
-import quoteLeft from "../assets/images/quoteLeft.png";
+import StrengthWeakness from "../components/StrengthWeakness";
 
 const Home = () => {
   useEffect(() => {
@@ -30,46 +28,7 @@ const Home = () => {
       <div data-aos="fade-up" className="mx-auto my-5 skills-rating">
         <Skills />
       </div>
-      <div data-aos="fade-up" className="strengths-weakness bg-info shadow">
-        <div className="strengths">
-          <Container fluid>
-            <h1 className="d-flex">
-              <Avatar src={quoteLeft} sx={{ height: 60, width: 60 }} />
-              &nbsp; <span className="my-auto">Strengths . . . </span>
-            </h1>
-            <ul className="my-5 my-md-4">
-              <li>sedee</li>
-              <li>sedee</li>
-              <li>sedee</li>
-              <li>sedee</li>
-              <li>sedee</li>
-            </ul>
-          </Container>
-        </div>
-        <div className="weakness">
-          <Container
-            fluid
-            className="text-end"
-            style={{
-              position: "absolute",
-              bottom: 20,
-              right: 20,
-            }}
-          >
-            <ul className="my-5 my-md-4">
-              <li>sedee</li>
-              <li>sedee</li>
-              <li>sedee</li>
-              <li>sedee</li>
-              <li>sedee</li>
-            </ul>
-            <h1 className="d-flex float-end">
-              <span className="my-auto">. . . Weakness </span>&nbsp;{" "}
-              <Avatar src={quoteRight} sx={{ height: 60, width: 60 }} />
-            </h1>
-          </Container>
-        </div>
-      </div>
+
       <Container>
         <div data-aos="fade-up" className="questions-section">
           where do you see yourself in 5 years
@@ -94,6 +53,8 @@ const Home = () => {
         </div>
         <div className="questions-section">hbhjbh</div>
       </Container>
+      <StrengthWeakness />
+      <div></div>
     </div>
   );
 };
