ad1188b7973124d33b3cb058cca5347aa921edd5
venkatesh-kumar-s <venkatesh.verto@gmail.com>
2024-06-09 18:12:40 +0530

    Added lazy to images

--- src/components/EducationSummary.js
+++ src/components/EducationSummary.js
@@ -6,7 +6,7 @@ const downloadIcon = (
     width="16"
     height="16"
     fill="currentColor"
-    class="bi bi-download"
+    className="bi bi-download"
     viewBox="0 0 16 16"
   >
     <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" />
@@ -18,43 +18,55 @@ const EducationSummary = ({ data }) => {
   return (
     <div className="edu-cont bg-success">
       <div className="vignette"></div>
-      <img src={data.banner} className="banner" />
-      <img src={data.logo} className={`icon ${data.type}-icon`} />
-
+      <img src={data.banner} className="banner" loading="lazy" />
+      <div>
+        <img
+          src={data.logo}
+          className={`icon ${data.type}-icon`} loading="lazy"
+        />
+      </div>
       <div
         className="d-flex col-10"
         style={{ position: "absolute", justifyContent: "space-between" }}
       >
-        <div class="card text-center section info">
-          <div class={`card-header h2 ${data.type}-title`}>
+        <div className="card text-center section info">
+          <div className={`card-header h2 ${data.type}-title`}>
             {data.institute}
           </div>
-          <div class="card-body">
-            <h5 class="card-title">Special title treatment</h5>
-            <p class="card-text">
+          <div className="card-body">
+            <h5 className="card-title">Special title treatment</h5>
+            <p className="card-text">
               With supporting text below as a natural lead-in to additional
               content.
             </p>
-            <a href="#" class="btn btn-primary">
-              Go somewhere
-            </a>
+            
           </div>
-          <div class="card-footer d-flex bg-info">
+          <div className="card-footer d-flex bg-info">
             <div className="col-4">{data.timeline}</div>|
             <div className="col-4">{data.course}</div>|
             <div className="col-4">{data.score + " " + data.scoreType}</div>
           </div>
         </div>
 
-        <div class="card text-center section files my-auto bg-light">
-          <div class={`card-header`}>Documents</div>
-          <div class="card-body p-0">
+        <div className="card text-center section files my-auto bg-light">
+          <div className={`card-header`}>Documents</div>
+          <div className="card-body p-0">
             <table className="table table-sm table-striped table-hover">
               <tbody>
-                {data.certificates.map((cert,idx)=><tr key={idx}>
-                  <td scope="col-auto">{cert.type}</td>
-                  <td scope="col-1"><a className="btn btn-link p-0" href={`/certificates/${cert.file}`} download={cert.file}>{downloadIcon}</a></td>
-                </tr>)}
+                {data.certificates.map((cert, idx) => (
+                  <tr key={idx}>
+                    <td scope="col-auto">{cert.type}</td>
+                    <td scope="col-1">
+                      <a
+                        className="btn btn-link p-0"
+                        href={`/certificates/${cert.file}`}
+                        download={cert.file}
+                      >
+                        {downloadIcon}
+                      </a>
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
